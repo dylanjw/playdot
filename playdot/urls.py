6dd6bd93f5e192244e2bd7fb0fb65f60adf36d06
Dylan Wilson <dylanjw@protonmail.com>
2021-10-05 12:10:41 -0700

    Add single player mode

--- playdot/urls.py
+++ playdot/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("board/create/<int:width>", views.create, name="create"),
+    path(
+        "board/create_sp/<int:width>", views.create_single_player, name="create"
+    ),
     path("list/", views.list_rooms, name="list_rooms"),
     path("board/<uuid:gid>/", views.room, name="room"),
     path("board/<uuid:gid>/state", views.game_state, name="game"),
